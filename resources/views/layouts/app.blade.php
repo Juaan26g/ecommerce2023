17fe18919e4767c020ae78c4038d70e0406f4266
Juaan26g <jgs26daw@gmail.com>
2023-01-13 23:33:43 +0100

    añadimos el tailwind para que funcione, como no he conseguido que funcione en local he decidido emplear el cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,9 +12,35 @@
 
         <!-- Styles -->
         @livewireStyles
-
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script>
+            const defaultTheme = require('tailwindcss/defaultTheme');
+            const colors = require('tailwindcss/colors');
+            module.exports = {
+                content: [
+                    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+                    './vendor/laravel/jetstream/**/*.blade.php',
+                    './storage/framework/views/*.php',
+                    './resources/views/**/*.blade.php',
+                ],
+            
+                theme: {
+                    extend: {
+                        fontFamily: {
+                            sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                        },
+                        colors: {
+                            trueGray: colors.neutral,
+                            orange: colors.orange,
+                            lime: colors.lime,
+                        }
+                    },
+                },
+            
+                plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+            };</script>
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
