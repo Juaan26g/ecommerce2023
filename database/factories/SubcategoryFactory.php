0adbaabd7182eb2f3278e3be81cfad11a738ace1
Juaan26g <jgs26daw@gmail.com>
2023-01-16 07:39:03 +0100

    WEEK 1 TASKS: test to check that the subcategories are seen when "Categorías" is clicked

--- database/factories/SubcategoryFactory.php
+++ database/factories/SubcategoryFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class SubcategoryFactory extends Factory
 {
@@ -13,7 +15,19 @@ class SubcategoryFactory extends Factory
      */
     public function definition()
     {
+
+       $category = Category::all()->random();
+       $name = $this->faker->word();
+        $slug = Str::slug($name);
+        $color = Collect([true, false])->random();
+        $size = Collect([true, false])->random();
+
         return [
+            'category_id' => $category->id,
+            'name' => $name,
+            'slug' => $slug,
+            'color' => $color,
+            'size' => $color ? $size : false,
         'image' => 'subcategories/' . $this->faker->image(storage_path('app/public/subcategories'), 640, 480, null, false)
         ];
     }
--- tests/Browser/CategoriesAndSubcategoriesTest.php
+++ tests/Browser/CategoriesAndSubcategoriesTest.php
@@ -3,14 +3,16 @@
 namespace Tests\Browser;
 
 use App\Models\Category;
+use App\Models\Subcategory;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
 class CategoriesAndSubcategoriesTest extends DuskTestCase
 {
+    use DatabaseMigrations;
     
-    /**@test*/
+    /** @test */
     public function categoriesAreSeenWhenCategoriesButtonIsClicked()
     {
         Category::factory()->create([
@@ -23,6 +25,28 @@ class CategoriesAndSubcategoriesTest extends DuskTestCase
             ->pause(500)
             ->clickLink('Categorías')
             ->assertSee('Consola y videojuegos');
+        });
+    }
+
+    /** @test */
+    public function subcategoriesAreSeenWhenCategoriesButtonIsClicked()
+    {
+        $category = Category::factory()->create([
+            'name' => 'Consola y videojuegos',
+        ]);
+
+        Subcategory::factory()->create([
+            'name' => 'Videojuegos para PC',
+            'category_id' => $category->id
+        ]);
+
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+            ->pause(500)
+            ->clickLink('Categorías')
+            ->assertSee('Consola y videojuegos')
+            ->assertSee('Videojuegos para PC');
         });
     }
 }
