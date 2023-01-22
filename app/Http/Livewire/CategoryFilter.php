f3234ffcaad75f6529e67dd6dc0c41cb30fe7cb8
Juaan26g <Juaan26g@gmail.com>
2023-01-22 23:55:27 +0000

    programming the filters of the category page

--- app/Http/Livewire/CategoryFilter.php
+++ app/Http/Livewire/CategoryFilter.php
@@ -4,13 +4,23 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use Livewire\WithPagination;
+use App\Models\Product;
+
 class CategoryFilter extends Component
 {
-    public $category;
+    public $category, $subcategoria, $marca;
 
     use WithPagination;
+
+    public function limpiar()
+
+    {
+        $this->reset(['subcategoria', 'marca']);
+    }
+
     public function render()
     {
+      
         $products = $this->category->products()->where('status', 2)->paginate(20);
 
         return view('livewire.category-filter', compact('products'));
