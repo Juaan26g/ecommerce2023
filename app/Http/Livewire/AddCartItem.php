19d6c7c35664019d8f9222e0d562728f54648511
Juaan26g <Juaan26g@gmail.com>
2023-01-23 17:16:39 +0000

    the button cant outstand the stock quantity

--- app/Http/Livewire/AddCartItem.php
+++ app/Http/Livewire/AddCartItem.php
@@ -7,6 +7,14 @@ use Livewire\Component;
 class AddCartItem extends Component
 {
     public $qty = 1;
+    public $product;
+    public $quantity;
+
+    public function mount()
+    {
+        $this->quantity = $this->product->quantity;
+    }
+
 
     public function decrement()
     {
