90b27cb3978b84432bc12029d37a688210329da0
Juaan26g <Juaan26g@gmail.com>
2023-01-23 00:05:24 +0000

    filters funtionality added, it doesnt seem to work, will need a revision when ended week 2

--- app/Http/Livewire/CategoryFilter.php
+++ app/Http/Livewire/CategoryFilter.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 use Livewire\WithPagination;
 use App\Models\Product;
+use Illuminate\Database\Eloquent\Builder;
 
 class CategoryFilter extends Component
 {
@@ -16,11 +17,24 @@ class CategoryFilter extends Component
 
     {
         $this->reset(['subcategoria', 'marca']);
-    }
+    }   
 
     public function render()
     {
-      
+        $productsQuery = Product::query()->whereHas('subcategory.category', function (Builder $query) {
+            $query->where('id', $this->category->id);
+        });
+        if ($this->subcategoria) {
+            $productsQuery = $productsQuery->whereHas('subcategory', function (Builder $query) {
+                $query->where('name', $this->subcategoria);
+            });
+        }
+        if ($this->marca) {
+            $productsQuery = $productsQuery->whereHas('brand', function (Builder $query) {
+                $query->where('name', $this->marca);
+            });
+        }
+        
         $products = $this->category->products()->where('status', 2)->paginate(20);
 
         return view('livewire.category-filter', compact('products'));
