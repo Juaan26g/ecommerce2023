3cc3b5cd26174fbe894d28ce99d42221247a61cd
Juaan26g <Juaan26g@gmail.com>
2023-01-23 17:58:52 +0000

    fixed some previous bugs , added the size and color selector to the size color products view

--- app/Http/Livewire/AddCartItemSize.php
+++ app/Http/Livewire/AddCartItemSize.php
@@ -3,9 +3,24 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Size;
 
 class AddCartItemSize extends Component
 {
+    public $product;
+    public $sizes;
+    public $size_id = '';
+    public $colors = [];
+    public function mount()
+    {
+        $this->sizes = $this->product->sizes;
+    }
+    public function updatedSizeId($value)
+    {
+        $size = Size::find($value);
+        $this->colors = $size->colors;
+    }
+
     public function render()
     {
         return view('livewire.add-cart-item-size');
