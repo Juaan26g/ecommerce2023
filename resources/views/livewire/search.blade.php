11895440caec19ff271e9cdf42021281c1df11c5
Juaan26g <Juaan26g@gmail.com>
2023-01-30 05:12:59 +0100

    the shown products are clickable and redirects to the product

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -6,23 +6,23 @@
         <x-search size="35" color="white"></x-search>
     </button>
 
-    <div class="absolute w-full mt-1 hidden" :class="{ 'hidden' : !$wire.open }" @click.away="$wire.open = false">
+    <div class="absolute w-full mt-1 hidden" :class="{ 'hidden': !$wire.open }" @click.away="$wire.open = false">
         <div class="bg-white rounded-lg shadow-lg">
-        <div class="px-4 py-3 space-y-1">
-            @forelse ($products as $product)
-                <div class="flex">
-                    <img class="w-16 h-12 object-cover" src="{{ Storage::url($product->images->first()->url) }}">
-                    <div class="ml-4 text-gray-700">
-                        <p class="text-lg font-semibold leading-5">{{ $product->name }}</p>
-                        <p>Categoria: {{ $product->subcategory->category->name }}</p>
-                    </div>
-                </div>
-            @empty
-                <p class="text-lg leading-5">
-                    No existe ningún registro con los parámetros especificados
-                </p>
-            @endforelse
+            <div class="px-4 py-3 space-y-1">
+                @forelse ($products as $product)
+                    <a href="{{ route('products.show', $product) }}" class="flex">
+                        <img class="w-16 h-12 object-cover" src="{{ Storage::url($product->images->first()->url) }}">
+                        <div class="ml-4 text-gray-700">
+                            <p class="text-lg font-semibold leading-5">{{ $product->name }}</p>
+                            <p>Categoria: {{ $product->subcategory->category->name }}</p>
+                        </div>
+                    </a>
+                @empty
+                    <p class="text-lg leading-5">
+                        No existe ningún registro con los parámetros especificados
+                    </p>
+                @endforelse
+            </div>
         </div>
     </div>
-</div>
 </div>
