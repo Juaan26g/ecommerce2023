28e4bbe7a7ba5608a91a2a3c172c0ae59217bc2c
Juaan26g <Juaan26g@gmail.com>
2023-01-30 23:37:10 +0100

    created listeners of the cart

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\{WelcomeController, CategoryController, ProductsController, SearchController};
-use App\Http\Livewire\ShoppingCart;
+use App\Http\Livewire\{ShoppingCart, CreateOrder };
 
 
 /*
