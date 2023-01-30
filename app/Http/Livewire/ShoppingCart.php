70976b84a5f5be3d4fcfd247afe200bc4a758f95
Juaan26g <Juaan26g@gmail.com>
2023-01-30 22:20:09 +0100

    function to delete products from the cart

--- app/Http/Livewire/ShoppingCart.php
+++ app/Http/Livewire/ShoppingCart.php
@@ -3,11 +3,17 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Gloudemans\Shoppingcart\Facades\Cart;
 
 class ShoppingCart extends Component
 {
     public $listeners = ['render'];
 
+    public function destroy()
+    {
+        Cart::destroy();
+        $this->emitTo('dropdown-cart', 'render');
+    }
     public function render()
     {
         return view('livewire.shopping-cart');
