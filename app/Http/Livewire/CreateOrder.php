e657b963a8805f3c6166cc04277d71f011768c89
Juaan26g <Juaan26g@gmail.com>
2023-01-31 21:04:58 +0100

    validation (pdf 17)

--- app/Http/Livewire/CreateOrder.php
+++ app/Http/Livewire/CreateOrder.php
@@ -8,10 +8,30 @@ use App\Models\Department;
 class CreateOrder extends Component
 {
 
-    public $departments, $cities =[], $districts=[];
+    public $departments, $cities = [], $districts = [];
     public $department_id = '', $city_id = '', $district_id = '';
     public $address, $reference;
+    public $contact, $phone;
     public $envio_type = 1;
+
+    public $rules = [
+        'contact' => 'required',
+        'phone' => 'required',
+        'envio_type' => 'required'
+    ];
+
+    public function create_order()
+    {
+        $rules = $this->rules;
+        if ($this->envio_type == 2) {
+            $rules['department_id'] = 'required';
+            $rules['city_id'] = 'required';
+            $rules['district_id'] = 'required';
+            $rules['address'] = 'required';
+            $rules['reference'] = 'required';
+        }
+        $this->validate($rules);
+    }
     public function mount()
     {
         $this->departments = Department::all();
