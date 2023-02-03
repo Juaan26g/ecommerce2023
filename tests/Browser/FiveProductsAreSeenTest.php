8d845099200376f4b8e09203123776939c2e996c
Juaan26g <Juaan26g@gmail.com>
2023-02-03 20:04:36 +0100

    WEEK 2 execise 3 (trying the use of collections in the test)

--- tests/Browser/FiveProductsAreSeenTest.php
+++ tests/Browser/FiveProductsAreSeenTest.php
@@ -39,25 +39,69 @@ class FiveProductsAreSeenTest extends DuskTestCase
                 'imageable_type' => Product::class
             ]);
         }
-       /* $Product1 = $products[0];
-        $Product2 = $products[1];
-        $Product3 = $products[2];
-        $Product4 = $products[3];
-        $Product5 = $products[4]; */
-        /* use($Product1,$Product2,$Product3,$Product4,$Product5)*/
+       
         $this->browse(function (Browser $browser) use ($products){
             $browser->visit('/')
             ->pause(500);
             foreach ($products as $product) {
                 $browser->assertSee($product->name);
             }
-            /*->assertSee($Product1->name)
-            ->assertSee($Product2->name)
-            ->assertSee($Product3->name)
-            ->assertSee($Product4->name)
-            ->assertSee($Product5->name)*/
+            
             
         
     });
+    }
+
+
+
+    /** @test */
+    public function publishedProductsAreSeenInTheMainView()
+    {
+        $category  = Category::factory()->create([
+            'name' => 'Consola y videojuegos',
+        ]);
+
+        $brand = Brand::factory()->create();
+        $category->brands()->attach($brand->id);
+
+        $subcategory = Subcategory::factory()->create([
+            'category_id' => $category->id,
+            'name' => 'Xbox'
+        ]);
+
+        Product::factory(3)->create([
+            'subcategory_id' => $subcategory->id,
+            
+        ]);
+
+        Product::factory(2)->create([
+            'subcategory_id' => $subcategory->id,
+            'status' => 1
+            
+        ]);
+        $products = Product::where('subcategory_id', $subcategory->id)->get();
+        foreach ($products as $product) {
+
+            Image::factory()->create([
+                'imageable_id' => $product->id,
+                'imageable_type' => Product::class
+            ]);
+        }
+       
+        $this->browse(function (Browser $browser) use ($products) {
+            $browser->visit('/')
+                    ->pause(500);
+        
+            $productsToSee = collect($products)->take(3);
+            $productsToDontSee = collect($products)->slice(3);
+        
+            foreach ($productsToSee as $product) {
+                $browser->assertSee($product->name);
+            }
+        
+            foreach ($productsToDontSee as $product) {
+                $browser->assertDontSee($product->name);
+            }
+        });
     }
 }
