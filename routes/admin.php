228e3cbfa929dd672a2cc0fabe38eda8ab4a3c1e
Juaan26g <jgs26daw@gmail.com>
2023-02-12 20:13:36 +0100

    creatin a create products button

--- routes/admin.php
+++ routes/admin.php
@@ -4,3 +4,4 @@ use App\Http\Livewire\Admin\{ShowProducts,};
 
 Route::get('/', ShowProducts::class)->name('admin.index');
 Route::get('products/{product}/edit', function () {})->name('admin.products.edit');
+Route::get('products/create', function () {})->name('admin.products.create');
