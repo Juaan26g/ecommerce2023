2bb56a6786cf9ecf57e8eb0ddb66bab87b76c991
Juaan26g <jgs26daw@gmail.com>
2023-02-13 00:39:12 +0100

    pdf 22 ended

--- app/Http/Livewire/Admin/EditProduct.php
+++ app/Http/Livewire/Admin/EditProduct.php
@@ -6,6 +6,7 @@ use App\Models\{Product, Category};
 use App\Models\Brand;
 use App\Models\Subcategory;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class EditProduct extends Component
@@ -21,6 +22,7 @@ class EditProduct extends Component
         'product.description' => 'required',
         'product.brand_id' => 'required',
         'product.price' => 'required',
+        'product.quantity' => 'numeric',
     ];
     public function mount(Product $product)
     {
@@ -33,9 +35,34 @@ class EditProduct extends Component
             $query->where('category_id', $this->category_id);
         })->get();
     }
+
+    public function updatedProductName($value)
+    {
+        $this->product->slug = Str::slug($value);
+    }
+    public function updatedCategoryId($value)
+    {
+        $this->subcategories = Subcategory::where('category_id', $value)->get();
+        $this->brands = Brand::whereHas('categories', function (Builder $query) use ($value) {
+            $query->where('category_id', $value);
+        })->get();
+        $this->product->subcategory_id = '';
+        $this->product->brand_id = '';
+    }
     public function getSubcategoryProperty()
     {
         return Subcategory::find($this->product->subcategory_id);
+    }
+    public function save()
+    {
+        $this->rules['product.slug'] = 'required|unique:products,slug,' . $this->product->id;
+        if ($this->product->subcategory_id) {
+            if (!$this->subcategory->color && !$this->subcategory->size) {
+                $this->rules['product.quantity'] = 'required|numeric';
+            }
+        }
+        $this->validate();
+        $this->product->save();
     }
     public function render()
     {
--- resources/views/livewire/admin/edit-product.blade.php
+++ resources/views/livewire/admin/edit-product.blade.php
@@ -1,6 +1,6 @@
 <div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-12 text-gray-700">
     <h1 class="text-3xl text-center font-semibold mb-8">Complete los datos para crear un producto</h1>
-    <div class="grid grid-cols-2 gap-6 mb-4">
+    <div class="bg-white shadow-xl rounded-lg p-6">
         <div>
             <x-jet-label value="Categorías" />
             <select class="w-full form-control" wire:model="category_id">
@@ -40,7 +40,7 @@
             <textarea class="w-full form-control" rows="4" wire:model="product.description" x-data x-init="ClassicEditor.create($refs.miEditor)
                 .then(function(editor) {
                     editor.model.document.on('change:data', () => {
-                        @this.set('description', editor.getData())
+                        @this.set('product.description', editor.getData())
                     })
                 })
                 .catch(error => {
@@ -68,14 +68,26 @@
             <x-jet-input-error for="product.price" />
         </div>
     </div>
-    @if (!$this->subcategory->color && !$this->subcategory->size)
+    @if ($this->subcategory && !$this->subcategory->color && !$this->subcategory->size)
         <div>
             <x-jet-label value="Cantidad" />
             <x-jet-input wire:model="product.quantity" type="number" class="w-full" />
             <x-jet-input-error for="product.quantity" />
         </div>
     @endif
-    <div class="flex mt-4">
-        ...
+    <div class="flex justify-end items-center mt-4">
+        <x-jet-action-message class="mr-3" on="saved">
+            Actualizado
+        </x-jet-action-message>
+        <x-jet-button wire:loading.attr="disabled" wire:target="save" wire:click="save" class="ml-auto">
+            Actualizar producto
+        </x-jet-button>
     </div>
+    @if ($this->subcategory)
+        @if ($this->subcategory->size)
+            @livewire('admin.size-product', ['product' => $product], key('size-product-' . $product->id))
+        @elseif($this->subcategory->color)
+            @livewire('admin.color-product', ['product' => $product], key('color-product-' . $product->id))
+        @endif
+    @endif
 </div>
