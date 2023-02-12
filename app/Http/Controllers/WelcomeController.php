5cf35e1b8e592f352dd2ddc14be23db3594ea02e
Juaan26g <jgs26daw@gmail.com>
2023-02-12 05:08:40 +0100

    Pay button orders.show

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -1,18 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\Category;
+
+use App\Models\{Category, Order};
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    public function __invoke()
- {
-   $categories = Category::all();
+   public function __invoke()
+   {
+      if (auth()->user()) {
+         $pendientes = Order::where('user_id', auth()->user()->id)->where('status', 1)->count();
+         if ($pendientes) {
+            $mensaje = "Tiene $pendientes ordenes pendientes de pago. <a class='font-bold' href='" . route('orders.index') .
+               "?status=1'>Pagar</a>";
+            session()->flash('flash.banner', $mensaje);
+         }
+      }
 
-    return view('welcome', compact('categories') );
- }
+      $categories = Category::all();
 
-
- 
+      return view('welcome', compact('categories'));
+   }
 }
