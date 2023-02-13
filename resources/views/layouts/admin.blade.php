0148ed9cd43afff29049055c82af3277e56f4c7a
Juaan26g <jgs26daw@gmail.com>
2023-02-13 19:17:34 +0100

    pdf 23 + sweeralert

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -14,6 +14,7 @@
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/sweetalert2/dist/sweetalert2.css') }}">
     
     
 
@@ -22,6 +23,7 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('vendor/ckeditor5/build/ckeditor.js') }}"></script>
+    <script src="{{ asset('vendor/sweetalert2/dist/sweetalert2.js') }}"></script>
 
 </head>
 
