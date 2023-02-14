5af8d56d5d3e34c35f156c7b861a41ca4deb6ff4
Juaan26g <jgs26daw@gmail.com>
2023-02-14 02:55:06 +0100

    delete images

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,9 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\Admin\{ShowProducts,EditProduct,CreateProduct};
+use App\Http\Controllers\Admin\ProductController;
 
 Route::get('/', ShowProducts::class)->name('admin.index');
 Route::get('products/{product}/edit', EditProduct::class)->name('admin.products.edit');
 Route::get('products/create', CreateProduct::class)->name('admin.products.create');
+Route::post('product/{product}/files', [ProductController::class, 'files'])->name('admin.products.files');
