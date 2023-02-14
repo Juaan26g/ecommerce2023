daa12bef26cebda9b189032ad1a10b668ba3f378
Juaan26g <jgs26daw@gmail.com>
2023-02-14 05:48:44 +0100

    pdf 25

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -17,6 +17,10 @@
                         Productos
                     </x-jet-nav-link>
                 </div>
+
+                <x-jet-nav-link href="{{ route('admin.categories.index') }}" :active="request()->routeIs('admin.categories.index')">
+                    Categorías
+                </x-jet-nav-link>
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
@@ -154,6 +158,10 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-jet-responsive-nav-link href="{{ route('admin.index') }}" :active="request()->routeIs('admin.index')">
                 Productos
+            </x-jet-responsive-nav-link>
+
+            <x-jet-responsive-nav-link href="{{ route('admin.categories.index') }}" :active="request()->routeIs('admin.categories.index')">
+                Categorías
             </x-jet-responsive-nav-link>
         </div>
 
--- routes/admin.php
+++ routes/admin.php
@@ -1,9 +1,10 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\Admin\{ShowProducts,EditProduct,CreateProduct};
-use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\{ProductController, CategoryController};
 
 Route::get('/', ShowProducts::class)->name('admin.index');
 Route::get('products/{product}/edit', EditProduct::class)->name('admin.products.edit');
 Route::get('products/create', CreateProduct::class)->name('admin.products.create');
 Route::post('product/{product}/files', [ProductController::class, 'files'])->name('admin.products.files');
+Route::get('categories', [CategoryController::class, 'index'])->name('admin.categories.index');
