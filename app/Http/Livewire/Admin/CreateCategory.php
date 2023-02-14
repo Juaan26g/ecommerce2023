27a10f4a3c50d6fa2b711aa66070f793dd137f5e
Juaan26g <jgs26daw@gmail.com>
2023-02-14 19:41:18 +0100

    validation on creation

--- app/Http/Livewire/Admin/CreateCategory.php
+++ app/Http/Livewire/Admin/CreateCategory.php
@@ -1,15 +1,47 @@
 <?php
 
 namespace App\Http\Livewire\Admin;
+
 use App\Models\Brand;
+use Illuminate\Support\Str;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class CreateCategory extends Component
 {
+    use WithFileUploads;
+
+    protected $validationAttributes = [
+        'createForm.name' => 'nombre',
+        'createForm.slug' => 'slug',
+        'createForm.icon' => 'icono',
+        'createForm.image' => 'imagen',
+        'createForm.brands' => 'marcas',
+        ];
+    protected $rules = [
+        'createForm.name' => 'required',
+        'createForm.slug' => 'required|unique:categories,slug',
+        'createForm.icon' => 'required',
+        'createForm.image' => 'required|image|max:1024',
+        'createForm.brands' => 'required',
+    ];
+
+    public $createForm = [
+        'name' => null,
+        'slug' => null,
+        'icon' => null,
+        'image' => null,
+        'brands' => [],
+    ];
     public $brands;
     public function mount()
     {
         $this->getBrands();
+    }
+
+    public function updatedCreateFormName($value)
+    {
+        $this->createForm['slug'] = Str::slug($value);
     }
     public function getBrands()
     {
@@ -17,6 +49,7 @@ class CreateCategory extends Component
     }
     public function save()
     {
+        $this->validate()
     }
     public function render()
     {
