9acb959f0bdd1eb5de7ce1393c3f67f2a68f84f9
Juaan26g <jgs26daw@gmail.com>
2023-02-20 00:11:27 +0100

    WEEK 3 exercise 6

---
+++ tests/Feature/Livewire/SearchTests.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Http\Livewire\Search;
+use App\Models\{Subcategory,Brand,Category,Image,Product};
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Livewire\Livewire;
+
+
+class SearchTests extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+
+    public function filtersByName()
+    {
+        $this->createProduct('Microfono');
+        $this->createProduct('Estetoscopio');
+
+        Livewire::test(Search::class)
+            ->set('search', 'Mic')
+            ->assertSee('Microfono')
+            ->assertDontSee('Estetoscopio');
+    }
+    /** @test */
+    public function ItDoesntShowAnyProductIfSearchInputIsEmpty()
+    {
+        $this->createProduct('Microfono');
+        $this->get('/');
+
+        Livewire::test(Search::class)
+            ->set('search', '')
+            ->assertDontSee('Microfono');
+    }
+    private function createProduct($name)
+    {
+        $category = Category::factory()->create();
+        $subcategory = Subcategory::factory()->create([
+            'category_id' => $category->id,
+            'color' => false,
+            'size' => false,
+        ]);
+
+        $brand = Brand::factory()->create();
+        $category->brands()->attach([$brand->id]);
+
+        $product = Product::factory()->create([
+            'subcategory_id' => $subcategory->id,
+            'name' => $name,
+            'brand_id' => $brand->id,
+            'quantity' => 2
+        ]);
+        Image::factory(2)->create([
+            'imageable_id' => $product->id,
+            'imageable_type' => Product::class
+        ]);
+        return $product;
+    }
+}
